ee4ef88cde137503e20b19410015f5907e466d9a
Prashant mishra <prashantmishra@Prashants-Air.lan>
2025-07-31 11:44:45 +0530

    fix: setup axiosClient and update GET handler for product queries

--- lib/axiosClient.ts
+++ lib/axiosClient.ts
@@ -1,9 +1,10 @@
-// lib/axiosClient.ts
 import axios from "axios";
 
-export const axiosClient = axios.create({
-  baseURL: 'https://strapi-backend-av9r.onrender.com',
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_STRAPI_URL + "/api",
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
+    Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`, // Optional
   },
 });
+
+export { axiosClient };
